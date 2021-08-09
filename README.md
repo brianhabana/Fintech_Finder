b690bd9f35ca52ba06a2e21628a511aa8a50e1a6
Brian Habana <brianhabana@gmail.com>
2021-08-08 21:21:18 -0700

    updated

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Fintech_Finder
+# Fintech Finder
+
+Fintech Finder is an application that its customers can use to find fintech professionals from among a list of candidates, hire them, and pay them.
+
+
+---
+
+## Technologies
+
+This project leverages python 3.7 with the following packages:
+
+* [streamlit](https://streamlit.io) - Streamlit turns data scripts into shareable web apps in minutes. 
+All in Python. All for free. No front‑end experience required
+
+* [web3](https://web3py.readthedocs.io/en/stable/overview.html) - Web3.py middleware is described using an onion metaphor, where each layer of middleware may affect both the incoming request and outgoing response from your provide
+
+
+---
+
+## Installation Guide
+
+Before running the application first install the following dependencies.
+
+```python
+  pip install streamlit
+  pip install web3==5.17
+
+```
+
+---
+
+## Usage
+
+To use to send Kovan Ethereum to placeholder fintech professional's wallet address based on wage **app.py** with:
+
+```python
+streamlit run app.py
+```
+
+---
+
+## Contributors
+
+Brought to you by UC Berkeley Extension.
+
+---
+
+## License
+
+MIT
+
+## Screenshots
+
+![alt text](https://github.com/brianhabana/PyChain_Ledger/blob/main/images/Screen%20Shot%202021-08-07%20at%209.41.02%20PM.png)
+![alt text](https://github.com/brianhabana/PyChain_Ledger/blob/main/images/Screen%20Shot%202021-08-07%20at%209.41.13%20PM.png)
--- fintech_finder.py
+++ fintech_finder.py
@@ -29,6 +29,7 @@
 import streamlit as st
 from dataclasses import dataclass
 from typing import Any, List
+from crypto_wallet import generate_account, get_balance, send_transaction
 
 ################################################################################
 # Step 1:
@@ -130,7 +131,7 @@ st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
 
 # @TODO:
 #  Call the `generate_account` function and save it as the variable `account`
-# YOUR CODE HERE
+account = generate_account()
 
 ##########################################
 
@@ -146,7 +147,7 @@ st.sidebar.write(account.address)
 # @TODO
 # Call `get_balance` function and pass it your account address
 # Write the returned ether balance to the sidebar
-# YOUR CODE HERE
+st.sidebar.write(get_balance(account.address))
 
 ##########################################
 
@@ -237,11 +238,10 @@ st.sidebar.markdown("## Total Wage in Ether")
 # Calculate total `wage` for the candidate by multiplying the candidate’s hourly
 # rate from the candidate database (`candidate_database[person][3]`) by the
 # value of the `hours` variable
-# YOUR CODE HERE
-
+wage = candidate_database[person][3] * hours
 # @TODO
 # Write the `wage` calculation to the Streamlit sidebar
-# YOUR CODE HERE
+st.sidebar.write(wage)
 
 ##########################################
 # Step 2 - Part 2:
@@ -268,8 +268,8 @@ if st.sidebar.button("Send Transaction"):
     # Call the `send_transaction` function and pass it 3 parameters:
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
-    # YOUR CODE HERE
-
+    transaction_hash = send_transaction(account, candidate_address, wage)
+    
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
 
